31efe1508b229d029df2e1eb3c983f0706f7958e
RnG <113661821+okon0miyaki@users.noreply.github.com>
2023-01-10 12:30:50 +0700

    Create gui.java

---
+++ gui.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javax.swing.JOptionPane;
+
+ // @author MSI
+ 
+public class GUI {
+
+    public static void main(String[] args) {
+        
+        String name = JOptionPane.showInputDialog("What is your name ?");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+        
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Your age ?"));
+        JOptionPane.showMessageDialog(null, "You're " + age + " Years Old");
+        
+        double height = Double.parseDouble(JOptionPane.showInputDialog("How Tall you are?"));
+        JOptionPane.showMessageDialog(null, "Your Height is " + height + " cm");
+        
+        JOptionPane.showMessageDialog(null, "See You Later !");
+    }
+    
+}
